57f5878c0c747a5778b2d388395ab2ba03b97ca5
Endri2001 <tuk25511@temple.edu>
2022-02-05 22:23:03 -0500

    finished testing the basics of components

--- practice1/index.js
+++ practice1/index.js
@@ -41,8 +41,21 @@ function getNum(){
 
 class NumPicker extends React.Component{
 	render(){
+		const num = getNum();
+		let msg;
+		if(num ===7){
+			msg = <div>
+					<h2>YOU WON!</h2>
+					<img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYuzhQ5bP29t_l9ljN0wdFY81iZbMv-s0FnIFCvnntMRpEj8JPG3jDRjeppfmS-oKs6Js&usqp=CAU"/> 
+				  </div>
+		}else{
+			msg = <p>you lose</p>
+		}
 		return(
-			<h1>Your number is:{getNum()}</h1>
+			<div>
+				<h1>Your number is:{num}</h1>
+				{msg}
+			</div>
 		)
 	}
 }
